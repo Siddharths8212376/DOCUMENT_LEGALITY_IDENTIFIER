c2a588597753a9cae8eee9d262c14cb523a8b0a0
Siddharths8212376 <siddharth8212376@gmail.com>
2019-09-09 16:15:45 +0530

    adding the partial definitions implementation

--- legal_data_collector.py
+++ legal_data_collector.py
@@ -20,13 +20,13 @@ soup = BeautifulSoup(contents, 'html.parser')
 words_and_meanings = {}
 all_paragraphs = soup.find_all('p', {'class': 'text'})
 all_strong_words = []
-# all_corr_meanings = []
+all_corr_meanings = []
 for each_paragraph in all_paragraphs:
     strong_words = each_paragraph.find('strong')
-    # words_meanings = each_paragraph.text.split(": ", 1)
+    words_meanings = each_paragraph.text.split(": ", 1)
     if strong_words is not None:
         all_strong_words.append(strong_words.text)
-        # all_corr_meanings.append(words_meanings)
+        all_corr_meanings.append(words_meanings)
 # print(len(all_strong_words))
 # print(all_corr_meanings)
 # print(all_strong_words)
@@ -55,16 +55,16 @@ for each_term in unclean_terms:
         legal_terms.append(each_term.strip())
         # stripping for removing all the leading and trailing spaces
     
-print(legal_terms, "\n", len(legal_terms))
+# print(legal_terms, "\n", len(legal_terms))
 
 # # add all the data into a csv file
 import csv
 
-with open("legal_terms.csv", mode='w') as legal_terms_file:
-    legal_writer = csv.writer(legal_terms_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    legal_writer.writerow(['LEGAL_TERMS'])
-    for each_term in legal_terms:
-        legal_writer.writerow([each_term])
+# with open("legal_terms.csv", mode='w') as legal_terms_file:
+#     legal_writer = csv.writer(legal_terms_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#     legal_writer.writerow(['LEGAL_TERMS'])
+#     for each_term in legal_terms:
+#         legal_writer.writerow([each_term])
 
 # for each_term, index in enumerate(legal_terms):
 #     for each_set in all_corr_meanings:
@@ -72,3 +72,17 @@ with open("legal_terms.csv", mode='w') as legal_terms_file:
 #             print(all_corr_meanings[index][1])
 
 # we've got 303 legal terms from the legal terms dictionary successfully :)
+
+meanings = []
+for each_word_meaning in all_corr_meanings:
+    if len(each_word_meaning) > 1:
+        meanings.append(each_word_meaning[1])
+        
+# for meaning in meanings:
+#     print(meaning)
+with open('partial_definitions.txt', mode='w') as partial_def_file:
+    def_writer = csv.writer(partial_def_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    def_writer.writerow(['Definitions '])
+    for meaning in meanings:
+        def_writer.writerow([meaning])
+    
