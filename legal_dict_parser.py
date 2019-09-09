5ad46943f7b9cd9cbc8a298efacf7531998e3a1b
Siddharths8212376 <siddharth8212376@gmail.com>
2019-09-10 02:08:01 +0530

    adding the complete legal dictionary parser

--- legal_dict_parser.py
+++ legal_dict_parser.py
@@ -5,6 +5,14 @@
 
 # we need to change the web_page for each alphabet 
 alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# open the file for writing the first row for assigning column names
+# further inside the loop we'll append to the file
+# import csv libarary
+import csv
+
+with open("complete_legal_dictionary.csv", mode='w') as legal_dictionary:
+    legal_writer = csv.writer(legal_dictionary, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    legal_writer.writerow(['TERMS', 'MEANINGS'])
 
 # importing all the required libraries
 import requests
@@ -14,44 +22,43 @@ import re
 # for each letter in alphabets, scrape the website for all the words
 # and add them to a csv file
 
-# first we shall implement the basics
-web_page = requests.get("https://dictionary.law.com/Default.aspx?letter=A")
+# now we'll implement the complele dictionary, at least I hope so
 
-# get all the contents
-contents = web_page.content
-soup = BeautifulSoup(contents, 'html.parser')
-# print(soup.prettify())
+for each_letter in alphabets:
+    web_page = requests.get("https://dictionary.law.com/Default.aspx?letter=" + each_letter)
 
-# now parsing the contents we need, ie, words and definitions
-all_words = soup.find_all('span', {'class': 'word'})
-all_meanings = soup.find_all('span', {'class': 'definition'})
-# print(len(all_words), " ", len(all_meanings))
-words_meanings = {}
-for word, meaning in zip(all_words, all_meanings):
-    word_val = word.a.text
-    meaning_val = meaning.text
-    del_vals = ["\r", "\n", "\t"]
-    for val in del_vals:
-        if val in word_val:
-            word_val = word_val.replace(val, "")
-        if val in meaning_val:
-            meaning_val = meaning_val.replace(val, "")
-    word_val = word_val.strip()
-    meaning_val = meaning_val.strip()
-    words_meanings[word_val] = meaning_val
-# there are 209 legal terms starting with A 
-# print(words_meanings)
-# for word in words_meanings:
-#     print(word + " : " + words_meanings[word])
+    # get all the contents
+    contents = web_page.content
+    soup = BeautifulSoup(contents, 'html.parser')
+    # print(soup.prettify())
 
-# add it to a csv file in append mode
-import csv
+    # now parsing the contents we need, ie, words and definitions
+    all_words = soup.find_all('span', {'class': 'word'})
+    all_meanings = soup.find_all('span', {'class': 'definition'})
+    # print(len(all_words), " ", len(all_meanings))
+    words_meanings = {}
+    for word, meaning in zip(all_words, all_meanings):
+        word_val = word.a.text
+        meaning_val = meaning.text
+        del_vals = ["\r", "\n", "\t"]
+        for val in del_vals:
+            if val in word_val:
+                word_val = word_val.replace(val, "")
+            if val in meaning_val:
+                meaning_val = meaning_val.replace(val, "")
+        word_val = word_val.strip()
+        meaning_val = meaning_val.strip()
+        words_meanings[word_val] = meaning_val
+    # there are 209 legal terms starting with A 
+    # print(words_meanings)
+    # for word in words_meanings:
+    #     print(word + " : " + words_meanings[word])
 
-with open("legal_dictionary.csv", mode='w') as legal_dictionary:
-    legal_writer = csv.writer(legal_dictionary, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    legal_writer.writerow(['TERMS', 'MEANINGS'])
-    for each_word in words_meanings:
-        legal_writer.writerow([each_word, words_meanings[each_word]])
-# let's test if it'll work
-# it worked
-# saving the test copy for future references
+    # add it to a csv file in append mode
+
+    with open("complete_legal_dictionary.csv", mode='a') as legal_dictionary:
+        legal_writer = csv.writer(legal_dictionary, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        for each_word in words_meanings:
+            legal_writer.writerow([each_word, words_meanings[each_word]])
+
